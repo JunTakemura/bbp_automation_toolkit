47bcc24d3115458b339eb0dd229b77f3794cd88a
Jun Takemura <76585599+JunTakemura@users.noreply.github.com>
2024-12-26 11:37:59 +0100

    add arg parse

--- h1_domain_collector.py
+++ h1_domain_collector.py
@@ -5,14 +5,43 @@ import sys
 import argparse
 import requests
 
+# Parse command line arguments
 def parse_args():
     parser = argparse.ArgumentParser(
-        prog='h1_domain_collector.py',
-        description='Collects HackerOne in-scope domains')
+        prog="h1_domain_collector.py",
+        description="Collects HackerOne in-scope domains")
     )
     parser.add_argument(
-        '=u', '--api-username',
+        "-u", "--api-username",
         required = True,
-        help = 'HackerOne API username (email address)'
+        help = "HackerOne API username (email address)"
     )
+
+    parser.add_argument(
+        "-t", "--api-token",
+        required = True,
+        help = "HackerOne API token"
+    )
+
+    parser.add_argument(
+        "-b", "--bounty",
+        default = True,
+        choices = ["True", "False"],
+        help = (
+            "When set to 'True', only collect programmes that offers bounty"
+            "When set to 'False', only collect programmes that does not offer bounty"
+            "Default: "True""
+        )
+    )
+
+    args = parser.parse_args()
+    return args
+
+# Retrieve program handles
+def get_program_handles(api_username, api_token, bounty):
+    handles = []
+    page = 1
+    while True:
+        url = "https://api.hackerone.com/v1/hackers/programs"
+
 
